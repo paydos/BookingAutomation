4937aef4bb5dcdc47d71c95d221a0c1ee29778b2
PyFan <darru2002@gmail.com>
2024-01-21 17:40:16 +0100

    minor improvement

--- lib/setup.py
+++ lib/setup.py
@@ -23,19 +23,15 @@ class AutoInstaller:
         
 
     def install(self):
-        self.logger.info('Starting module installation/check process...')
         subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-q', '-r', self.requirements_file])
-        self.logger.info('Installation/check completed successfully.')
         
 
     def add_to_startup(self):
         try:
             self.file_path = os.path.join(self.file_path,'__main__.py' )                
             bat_path = r'C:\Users\%s\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup' % self.USER_NAME
-            self.logger.info('Adding BookingAutomation to run on startup')
             with open(bat_path + '\\' + "BookingAutomation.bat", "w+") as bat_file:
                 bat_file.write(r'python "%s"' % self.file_path)
-            self.logger.info('Script succesfully atttached to boot.')
         except Exception as e:
             self.logger.error('Failed to add script to startup: ' + str(e.__class__.__name__))
         
