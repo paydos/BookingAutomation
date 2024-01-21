4d898d1340fea30c5607ebd5a8ea907dc8e63bc6
PyFan <darru2002@gmail.com>
2024-01-21 17:42:52 +0100

    fixed bug

--- installer.py
+++ installer.py
@@ -1,6 +1,8 @@
 from lib.setup import AutoInstaller
-from lib.paths import install_log_path
+from lib.paths import install_log_path, log_folder
+
 import logging
+import os
 
 # Configure logging
 logging.basicConfig(filename=install_log_path, level=logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s')
@@ -17,3 +19,11 @@ logging.info('Installation of required modules completed.')
 logging.info('Adding application to system startup.')
 installer.add_to_startup()
 logging.info('Application added to system startup successfully.')
+
+
+if not os.path.exists(log_folder):
+    os.makedirs(log_folder)
+    logging.info('Log folder created at {}'.format(log_folder))
+else:
+    logging.info('Log folder already exists at {}'.format(log_folder))
+
--- lib/paths.py
+++ lib/paths.py
@@ -3,3 +3,4 @@ import os
 log_file_path = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'log', 'BookingAutomation.log')
 requirements_file_path = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'requirements.txt')
 install_log_path = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'log', 'Installation.log')
+log_folder = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'log')
